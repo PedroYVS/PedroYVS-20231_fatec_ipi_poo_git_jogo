9ee78847872c0cf7750d0ab6f52fed4eed82fea9
Pedro Yoshio <pedro.shibayama@fatec.sp.gov.br>
2023-03-09 15:32:27 -0300

    Primeiro commit GitIgnore e Personagem.java

---
+++ Personagem.java
@@ -0,0 +1,24 @@
+//Em Java chama-se método em vez de função. Métodos possuem tipo de retorno, nome de chamada,...
+//... conjunto de parâmetros e corpo.
+
+public class Personagem{
+    //Isso é uma variável de instância.
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    //Isso é um método de instância.
+    void cacar(){
+        System.out.println(nome + " caçando");
+    }
+
+    void comer(){
+        System.out.print(nome + " comendo\n");
+    }
+
+    void dormir(){
+        //"%n" é o pula linha para qualquer SO que for (Linux, Windows etc.).
+        System.out.printf("%s dormindo%n", nome);
+    }
+}
